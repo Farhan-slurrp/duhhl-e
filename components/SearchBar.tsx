e6211cda606e586d2c00c4682dacc4fc0ebefaf0
Farhan Ahmad Nurzi <58872254+Farhan-slurrp@users.noreply.github.com>
2023-01-04 12:46:08 +0800

    initial commit

---
+++ components/SearchBar.tsx
@@ -0,0 +1,59 @@
+import React, { Dispatch, SetStateAction, useState } from "react";
+import { openai } from "../utils/initializeOpenAI";
+
+interface ISearchBarProps {
+  setImage: Dispatch<SetStateAction<string[]>>;
+  setLoading: Dispatch<SetStateAction<boolean>>;
+}
+
+const SearchBar: React.FC<ISearchBarProps> = ({ setImage, setLoading }) => {
+  const [prompt, setPrompt] = useState<string>("");
+
+  const handleClick = async () => {
+    if (prompt) {
+      setLoading(true);
+      try {
+        const response = await openai.createImage({
+          prompt: prompt,
+          n: 6,
+          size: "256x256",
+        });
+
+        if (response) {
+          setImage(
+            response.data?.data.map((img) => {
+              if (typeof img.url == "string") {
+                return img.url;
+              } else return "";
+            })
+          );
+          setLoading(false);
+        }
+      } catch (e) {
+        setImage([]);
+        setLoading(false);
+      }
+    }
+  };
+
+  return (
+    <div className="w-full md:w-2/3">
+      <div className="flex items-center gap-1 md:justify-center">
+        <input
+          type="text"
+          className="border-2 text-gray-700 border-black p-2 w-full shadow-lg"
+          placeholder="Type a prompt..."
+          onChange={(e) => setPrompt(e.target.value)}
+        />
+        <button
+          onClick={handleClick}
+          className="bg-black text-white p-2 font-bold border-2 shadow-lg border-black"
+        >
+          Search
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SearchBar;
